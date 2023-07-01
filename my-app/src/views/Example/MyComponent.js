68d17d0b7098cd1f4f9185686161525bcd89c87a
thanhbui96 <buihuythanh33@gmail.com>
2023-07-02 05:59:12 +0700

    het bai 9 form

--- my-app/src/views/Example/MyComponent.js
+++ my-app/src/views/Example/MyComponent.js
@@ -1,44 +1,74 @@
 
 import React from 'react';
 class MyComponent extends React.Component {
-    state = {
-        name: '',
-        channel: 'hoi dan it'
+    // state = {
+    //     name: '',
+    //     channel: 'hoi dan it'
+    // }
+
+    // handleOnChangeName = (event) => {
+    //     this.setState(
+    //         {
+    //             name: event.target.value,
+    //         }
+    //     )
+    // }
+    // handleClickButton = () => {
+    //     alert("click me")
+    // }
+    handleSubbmit = (event) => {
+        event.preventDefault()
+        alert("click me")
     }
-    
-    handleOnChangeName = (event) => {
+    state = {
+        firstname: '',
+        lastname: ''
+    }
+    handleChangeFirstName = (event) => {
         this.setState(
             {
-                name:event.target.value,
+                firstname: event.target.value,
+                lastname: event.target.value,
             }
         )
     }
-    handleClickButton = () => {
-        alert("click me")
+    handleChangeLastName = (event) => {
+        this.setState(
+            {
+                
+                lastname: event.target.value,
+            }
+        )
     }
     render() {
-        let name = "Eric"
+        //let name = "Eric"
         return (
             <>
 
 
-                <div className="abc"> hello bat dau {this.state["name"]} component
-                    {name}
-                    {console.log("thanhhhhhh", name)}
-                </div>
                 
-                <div className="first">
-                    <input value={this.state.name} type="text"
-                        onChange={(event) => this.handleOnChangeName(event)}
-                    />
-                    My name is {this.state['name']}
-                </div>
-                <div className="addd">
-                    gggggggggggggggggg {this.state.channel}
-                </div>
-                <div className="third">
-                    <button onClick={() => this.handleClickButton()}>click me</button>
-
+                <div>
+                    hello from
+                    <form>
+                        <label htmlFor="fname">First name:</label><br />
+                        <input
+                            type="text"
+                            value={this.state.firstname}
+                            onChange={(event) => this.handleChangeFirstName(event)}
+                        />
+                        <br />
+                        <label htmlFor="fname">Last name:</label><br />
+                        <input
+                            type="text"
+                            value={this.state.lastname}
+                            onChange={(event) => this.handleChangeLastName(event)}
+                        />
+                        <br />
+                        <br />
+                        <input type="button" value="Submit" 
+                        onClick={(event)=> this.handleSubbmit(event)}
+                        />
+                    </form>
                 </div>
 
             </>
